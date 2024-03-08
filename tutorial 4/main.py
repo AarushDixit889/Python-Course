27c761701cbfbd0112b42186b77cb2e36a46c1ae
unknown <Aarush Dixit>
2024-03-08 22:24:43 +0530

    Tutorial 4

---
+++ tutorial 4/main.py
@@ -0,0 +1,20 @@
+# Operators in python
+# There are four kinds of operations:
+# Arthimetic operation which consists of +,-,*,/,%,//,**
+
+a=int(input("Enter the value of a\n"))
+b=int(input("Enter the value of b\n"))
+# Addition
+print(f"{a} + {b} = {a+b}")
+# Subtraction
+print(f"{a} - {b} = {a-b}")
+# Multiplication
+print(f"{a} X {b} = {a*b}")
+# Division
+print(f"{a}/{b} = {a/b}")
+# Modulas
+print(f"{a} remainder {b} = {a%b}")
+# Double Slash
+print(f"{a} Double Slash {b} = {a//b}")
+# Power
+print(f"{a} to the power of {b} is {a**b}")
