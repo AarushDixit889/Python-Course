c324948c8e61d4990010e975687e2455629d552a
unknown <Aarush Dixit>
2024-03-08 22:22:00 +0530

    Tutorial 3

---
+++ tutorial 3/__main__.py
@@ -0,0 +1,11 @@
+# Variables in python
+# Name = value
+# Data types - Integer, Strings, Boolean
+# Integer - numbers like 22,7,8,19,292,10000000,5475498569868
+# Strings - text values like aarush,computer,python,i love python
+# Boolean - True,False
+a=22
+b="My name is aarush"
+c=True
+print(a,b,c,sep="""
+""")
